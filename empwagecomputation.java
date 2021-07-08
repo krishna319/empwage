0bf8247ecba339c480f8374235ad9eb83708f1ae
krishna319 <km83843@gmail.com>
2021-07-08 12:31:04 +0530

    welcome message

---
+++ empwagecomputation.java
@@ -0,0 +1,10 @@
+public class empwagecomputation {
+
+        public static void main(String args[]) {
+
+                System.out.println("Welcome to Employee Wage Computation Program");
+       }
+
+
+
+}
