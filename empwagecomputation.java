b9f2827b743be4af936d454289b5c0a397f27e88
krishna319 <km83843@gmail.com>
2021-07-08 12:56:07 +0530

    check emp attendence

--- empwagecomputation.java
+++ empwagecomputation.java
@@ -1,8 +1,22 @@
+import java.util.Random;
 public class empwagecomputation {
 
         public static void main(String args[]) {
 
                 System.out.println("Welcome to Employee Wage Computation Program");
+ 		Random num = new Random ();
+                int CheckAvail= num.nextInt(2);
+                System.out.println(" CheckAvail is " + CheckAvail);
+
+                if (CheckAvail == 1){
+
+			System.out.println("Employee is Present");
+                	}
+
+                else {
+                         System.out.println("Employee is Absent");
+
+                }
        }
 
 
